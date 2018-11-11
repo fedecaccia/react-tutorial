a909fdc7f01df43c7120280a91cc0aee4cf1e988
fedecaccia <federicoagustincaccia@gmail.com>
2018-11-11 19:25:26 -0300

    Updates

---
+++ precache-manifest.7469377356ae2da6f7589bd6016a429d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "5ac812a9d3c7d224e3b8",
+    "url": "/react-tutorial/static/js/main.5ac812a9.chunk.js"
+  },
+  {
+    "revision": "1670935a042b1fdc38db",
+    "url": "/react-tutorial/static/js/1.1670935a.chunk.js"
+  },
+  {
+    "revision": "5ac812a9d3c7d224e3b8",
+    "url": "/react-tutorial/static/css/main.87ceec2f.chunk.css"
+  },
+  {
+    "revision": "829d7c832b2d42a5802e1985f86fac5e",
+    "url": "/react-tutorial/index.html"
+  }
+];
